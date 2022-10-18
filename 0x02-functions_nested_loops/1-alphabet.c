40cb0559e84960b4472d9d44bd22e6cb933bc1b3
nnadi kingsley <kingsley20673gsley20673@gmail.com>
2022-10-18 09:47:00 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * main - print the alphabelt
+ * print_alphabet - print the alphabet
  * BY: NNADI KINGSLEY
  * Return 0.
  */
@@ -9,6 +9,8 @@ void print_alphabet(void)
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
+	{
 		_putchar(letter);
+	}
 	_putchar('\n');
 }
