ed5b9836ddc95d55eeeac9ed9f7252568791222d
nnadi kingsley <kingsley20673gsley20673@gmail.com>
2022-11-03 06:31:26 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,10 +1,12 @@
 #include "main.h"
 /**
-* _sqrt_recursion - find the square of a given number
-*@n: the given number
-*
-*Return: 0.
-*/
+ * sqrt2 - Makes possible to evaluate from 1 to n
+ * @a: same number as n
+ * @b: number that iterates from 1 to n
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 
 int sqrt2(int a, int b)
 {
