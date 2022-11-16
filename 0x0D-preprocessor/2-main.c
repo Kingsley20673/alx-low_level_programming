5a60f02e86c2d25510743fc945094d965c090c4e
nnadi kingsley <kingsley20673gsley20673@gmail.com>
2022-11-15 19:15:22 -0800

    preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - Print out the name of the file just compiled
+*
+*Return: 0 if code is ok
+*
+*/
+
+int main(void)
+{
+	printf("%s\n", _BASE_FILE_);
+
+	return (10);
+}
