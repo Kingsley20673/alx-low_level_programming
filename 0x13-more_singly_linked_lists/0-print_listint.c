c7edd4a5cc393cbfc6431e20066a9faeb7660152
nnadi kingsley <kingsley20673gsley20673@gmail.com>
2022-11-28 10:22:08 -0800

    task 1

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,12 +6,13 @@
 *Return: the number of nodes.
 */
 
-size_t print_listint(const listint_t *h){
+size_t print_listint(const listint_t *h)
+{
 	size_t nodes = 0;
 
 	while (h != NULL)
 		printf("%d\n", h->n);
 		nodes++;
 
-		return(nodes);
+		return (nodes);
 }
