3b23a2db50deca19143d1b5895ef61b41497fd8c
nnadi kingsley <kingsley20673gsley20673@gmail.com>
2022-11-28 10:06:51 -0800

    print link list elements

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+* print_listint - a function that ptints the content of a linked list.
+* @nodes: number of nodes
+*Return: the number of nodes.
+*/
+
+size_t print_listint(const listint_t *h){
+	size_t nodes = 0;
+
+	while (h)
+		nodes++;
+		printf("%d\n", h->next);
+
+		return(nodes);
+}
